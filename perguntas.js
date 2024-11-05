a233f80500437082891d1028c70e11bf67520f7e
ninipew <161456867+ninipew@users.noreply.github.com>
2024-11-05 11:50:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,45 @@
+criaCartao(
+    'Animais',
+    'quantos anos uma baleia azul vive?',
+    '80 a 90 anos'
+)
+criaCartao(
+    'química',
+    'qual o nome dado a geometria molecular do gás carbonico?',
+    'linear'
+)
+criaCartao(
+    'anatomia',
+    'qual o nome dado aos segmentos osseos dos dedos das mãos e dos pés?',
+    'falange'
+)
+criaCartao(
+    'artes',
+    'qual o nome dado ao ultimo ato de uma peça de teatro?',
+    'epílogo'
+)
+criaCartao(
+    'lingua portuguesa',
+    'qual é o plural de cristão?',
+    'cristãos'
+)
+criaCartao(
+    'quimica/fisica',
+    'qual o experimento mais famoso de RutherFord?',
+    'lamina de ouro'
+)
+criaCartao(
+    'história',
+    'qual o tratado celebrado em 1703 entre portugaal e inglaterra?',
+    'tratado de methuen'
+)
+criaCartao(
+    'persinalidade',
+    'qual nome se da a pessoa que tem gestos precipitados os desajeitados?',
+    'estabanada'
+)
+criaCartao(
+    'animais',
+    'uma animal que mata outro para se alimentar é?',
+    'predador'
+)
